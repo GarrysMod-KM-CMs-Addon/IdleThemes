91c8ee899149342aa482d3d1743b00d32d670c84
KM_CM <send.nex@gmail.com>
2025-12-18 09:27:31 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+An implementation of the Far Cry 3 idle themes we all know and love.
+
+Credits to Ubisoft Montreal for the theme from Far Cry 3,
+and to Sraigius for ripping the theme from the game files and uploading it to YouTube.
+
+A Moment Of Loneliness: https://youtu.be/_yjonDLdDxs?si=Y2e-NUVe5Khye3-t
+Lone Traveler: https://youtu.be/lmHsAedZqkE?si=SvsaOeDMucKyLPwp
+No Escape From Insanity: https://youtu.be/QH3Y3REEICI?si=C00BZjLisTVl2UlM
+A Long Road Ahead: https://youtu.be/V-LXgiZptNM?si=WfH7JBXl7wjnDmgJ
+Voices In The Distance: https://youtu.be/yXZM6wbhuLM?si=hxp7mNs55Z3k5Jck
---
+++ lua/autorun/client/KM_CMs_Addon_SoundTrack_IdleThemes.lua
@@ -0,0 +1,12 @@
+ include "autorun/client/Director.lua"
+
+Director_Music( "MUS_AMomentOfLoneliness", "Music/AMomentOfLoneliness.mp3" )
+DIRECTOR_MUSIC_IDLE_SEQUENCES.MUS_AMomentOfLoneliness = true
+Director_Music( "MUS_LoneTraveler", "Music/LoneTraveler.mp3" )
+DIRECTOR_MUSIC_IDLE_SEQUENCES.MUS_LoneTraveler = true
+Director_Music( "MUS_NoEscapeFromInsanity", "Music/NoEscapeFromInsanity.mp3" )
+DIRECTOR_MUSIC_IDLE_SEQUENCES.MUS_NoEscapeFromInsanity = true
+Director_Music( "MUS_ALongRoadAhead", "Music/ALongRoadAhead.mp3" )
+DIRECTOR_MUSIC_IDLE_SEQUENCES.MUS_ALongRoadAhead = true
+Director_Music( "MUS_VoicesInTheDistance", "Music/VoicesInTheDistance.mp3" )
+DIRECTOR_MUSIC_IDLE_SEQUENCES.MUS_VoicesInTheDistance = true
